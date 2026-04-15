26b9522bf0117649baf974574b69b40fd7d02560
machine <athulajaikumar@gmail.com>
2026-04-15 12:52:20 +0200

    Initial commit: Ledger (Qt Trading Journal) (C++ + SQLite)

---
+++ CMakeFiles/Ledger.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/machine/Ledger/build/Ledger_autogen/mocs_compilation.cpp" "CMakeFiles/Ledger.dir/Ledger_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/Ledger.dir/Ledger_autogen/mocs_compilation.cpp.o.d"
+  "/home/machine/Ledger/core/database.cpp" "CMakeFiles/Ledger.dir/core/database.cpp.o" "gcc" "CMakeFiles/Ledger.dir/core/database.cpp.o.d"
+  "/home/machine/Ledger/core/stats.cpp" "CMakeFiles/Ledger.dir/core/stats.cpp.o" "gcc" "CMakeFiles/Ledger.dir/core/stats.cpp.o.d"
+  "/home/machine/Ledger/core/trade.cpp" "CMakeFiles/Ledger.dir/core/trade.cpp.o" "gcc" "CMakeFiles/Ledger.dir/core/trade.cpp.o.d"
+  "/home/machine/Ledger/main.cpp" "CMakeFiles/Ledger.dir/main.cpp.o" "gcc" "CMakeFiles/Ledger.dir/main.cpp.o.d"
+  "/home/machine/Ledger/ui/addtradedialog.cpp" "CMakeFiles/Ledger.dir/ui/addtradedialog.cpp.o" "gcc" "CMakeFiles/Ledger.dir/ui/addtradedialog.cpp.o.d"
+  "/home/machine/Ledger/ui/mainwindow.cpp" "CMakeFiles/Ledger.dir/ui/mainwindow.cpp.o" "gcc" "CMakeFiles/Ledger.dir/ui/mainwindow.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Ledger.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ledger_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Ledger_autogen.dir/ParseCache.txt"
+  "Ledger_autogen"
+  "CMakeFiles/Ledger.dir/Ledger_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Ledger.dir/Ledger_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/Ledger.dir/core/database.cpp.o"
+  "CMakeFiles/Ledger.dir/core/database.cpp.o.d"
+  "CMakeFiles/Ledger.dir/core/stats.cpp.o"
+  "CMakeFiles/Ledger.dir/core/stats.cpp.o.d"
+  "CMakeFiles/Ledger.dir/core/trade.cpp.o"
+  "CMakeFiles/Ledger.dir/core/trade.cpp.o.d"
+  "CMakeFiles/Ledger.dir/main.cpp.o"
+  "CMakeFiles/Ledger.dir/main.cpp.o.d"
+  "CMakeFiles/Ledger.dir/ui/addtradedialog.cpp.o"
+  "CMakeFiles/Ledger.dir/ui/addtradedialog.cpp.o.d"
+  "CMakeFiles/Ledger.dir/ui/mainwindow.cpp.o"
+  "CMakeFiles/Ledger.dir/ui/mainwindow.cpp.o.d"
+  "Ledger"
+  "Ledger.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ledger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
